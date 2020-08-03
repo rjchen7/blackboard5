a070d4065f5718aeaf27dd8f3c737673568ebb36
ayw11 <ayw11@duke.edu>
2020-08-03 12:12:29 -0400

    used modern syntax in Category

--- components/Category.tsx
+++ components/Category.tsx
@@ -1,31 +1,31 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
+import { ImageSourcePropType } from 'react-native';
 
-class Category extends Component<any> {
-  render(this) {
-    return (
-      <View
-        style={{
-          height: 130,
-          width: 130,
-          marginLeft: 20,
-          borderWidth: 0.5,
-          borderColor: '#dddddd',
-        }}
-      >
-        <View style={{ flex: 2 }}>
-          <Image
-            source={this.props.imageUri}
-            style={{ flex: 1, resizeMode: 'cover' }}
-          />
-        </View>
-        <View style={{ flex: 1, paddingLeft: 10, paddingTop: 10 }}>
-          <Text>{this.props.name}</Text>
-        </View>
+type Props = {
+  imageUri: ImageSourcePropType;
+  name: string;
+};
+const Category = ({ imageUri, name }: Props) => {
+  return (
+    <View
+      style={{
+        height: 130,
+        width: 130,
+        marginLeft: 20,
+        borderWidth: 0.5,
+        borderColor: '#dddddd',
+      }}
+    >
+      <View style={{ flex: 2 }}>
+        <Image source={imageUri} style={{ flex: 1, resizeMode: 'cover' }} />
       </View>
-    );
-  }
-}
+      <View style={{ flex: 1, paddingLeft: 10, paddingTop: 10 }}>
+        <Text>{name}</Text>
+      </View>
+    </View>
+  );
+};
 export default Category;
 const styles = StyleSheet.create({
   container: {
