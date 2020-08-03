5b72c6887bbcc2ff7fa9dbc561b581a137f82e9b
ayw11 <ayw11@duke.edu>
2020-08-03 16:52:07 -0400

    profile

--- ProfileScreen.tsx
+++ ProfileScreen.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import {
+  Icon,
+  Container,
+  Content,
+  Header,
+  Left,
+  Body,
+  Right,
+  Button,
+} from 'native-base';
 const ProfileScreen = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Profile Screen</Text>
-    </View>
+    <Container style={{ flex: 1, backgroundColor: 'white' }}>
+      <Header>
+        <Left>
+          <Icon name='md-person-add' style={{ paddingLeft: 10 }}></Icon>
+        </Left>
+        <Body>
+          <Text style={{ fontWeight: '800' }}>Blackboard</Text>
+        </Body>
+        <Right>
+          <Icon name='ios-beer' style={{ paddingRight: 10 }}></Icon>
+        </Right>
+      </Header>
+      <Content>
+        <View>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={{ flex: 1 }}>
+              <Image
+                source={require('./assets/trick2g.jpg')}
+                style={{ width: 75, height: 75, borderRadius: 37.5 }}
+              ></Image>
+            </View>
+          </View>
+        </View>
+      </Content>
+    </Container>
   );
 };
+
+// const styles = StyleSheet.create({
+//   container: {
+
+//   }
+// });
 
 export default ProfileScreen;
