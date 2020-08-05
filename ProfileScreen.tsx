bf7c66e6dfb406d934562d13e5f1d41d6358f810
rjchen7 <ryanchen.fl@gmail.com>
2020-08-04 17:19:24 -0700

    nicer ProfileScreen

--- ProfileScreen.tsx
+++ ProfileScreen.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import {
-  Container,
-  Content,
-  Header,
-  Left,
-  Body,
-  Right,
-  Button,
-  Image,
-} from 'native-base';
+import { View, Text, Image } from 'react-native';
+import { Button } from 'native-base';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
@@ -20,50 +10,42 @@ const ProfileScreen = ({ navigation }) => {
   //   setActive(index);
   // };
   return (
-    <Container style={{ flex: 1, backgroundColor: 'white' }}>
-      <Content>
-        <View style={{ marginLeft: 15 }}>
-          <View style={{ flexDirection: 'row' }}>
-            <View style={{ flex: 1, alignItems: 'center' }}>
-              <Image
-                source={require('./assets/boomer2g.png')}
-                style={{ width: 75, height: 75, borderRadius: 37.5 }}></Image>
-            </View>
-            <Button
-              bordered
-              dark
-              style={{
-                flex: 3,
-                marginLeft: 10,
-                justifyContent: 'center',
-                height: 30,
-              }}>
-              <Text>Edit Profile</Text>
-            </Button>
-          </View>
-        </View>
-
-        <View>
-          <View
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
+      <View style={{ marginLeft: 15, marginTop: 20, marginBottom: 20 }}>
+        <View style={{ flexDirection: 'row' }}>
+          <Image
+            source={require('./assets/boomer2g.png')}
+            style={{ width: 75, height: 75, borderRadius: 37.5 }}
+          />
+          <Button
+            bordered
+            dark
             style={{
-              flexDirection: 'row',
-              justifyContent: 'space-around',
-              borderTopWidth: 1,
-              borderTopColor: '#eae5e5',
+              flex: 3,
+              marginLeft: 20,
+              marginRight: 30,
+              justifyContent: 'center',
+              height: 30,
             }}>
-            <Button transparent>
-              <Icon name='ios-clipboard'></Icon>
-            </Button>
-            <Button transparent>
-              <Icon name='ios-expand'></Icon>
-            </Button>
-            <Button transparent>
-              <Icon name='ios-clock'></Icon>
-            </Button>
-          </View>
+            <Text>Edit Profile</Text>
+          </Button>
         </View>
-      </Content>
-    </Container>
+      </View>
+
+      <View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            borderTopWidth: 1,
+            borderTopColor: '#eae5e5',
+          }}>
+          <Icon name='ios-clipboard' onPress={() => {}} />
+          <Icon name='ios-expand' onPress={() => {}} />
+          <Icon name='ios-clock' onPress={() => {}} />
+        </View>
+      </View>
+    </View>
   );
 };
 
